16e7c037dc75bbf6e80c22583914922130f9dbc6
DIVISIONSolar <me@joshsevero.dev>
2024-12-16 13:27:22 -0500

    push

--- wireguard.sh
+++ wireguard.sh
@@ -134,6 +134,7 @@ new_client_dns () {
 	echo "   4) OpenDNS"
 	echo "   5) Quad9"
 	echo "   6) AdGuard"
+	echo "   7) Local DNS Server (AGH)"
 	echo -e -n "${nocolor}"
 	read -p "DNS server [1]: " dns
 	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
@@ -169,6 +170,8 @@ new_client_dns () {
 		;;
 		6)
 			dns="94.140.14.14, 94.140.15.15"
+		;;
+		7) dns="10.7.0.1"
 		;;
 	esac
 }
